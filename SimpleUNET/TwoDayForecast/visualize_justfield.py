48cbd97a5b5f8598a167804ea2389decbb6d3594
Are Frode Kvanum <arefk@met.no>
2023-01-11 10:29:39 +0000

    updated predicttoin visualize to create no-land visuals

--- SimpleUNET/TwoDayForecast/visualize_justfield.py
+++ SimpleUNET/TwoDayForecast/visualize_justfield.py
@@ -14,6 +14,7 @@ import matplotlib as mpl
 
 from matplotlib import pyplot as plt
 from cartopy import crs as ccrs
+from cartopy import feature as cfeature
 from shapely.errors import ShapelyDeprecationWarning
 from cartopy.mpl.gridliner import LATITUDE_FORMATTER, LONGITUDE_FORMATTER
 
@@ -74,7 +75,7 @@ def main():
         init_date = datetime.strptime(yyyymmdd, '%Y%m%d')
         init_date = (init_date - timedelta(days = 2)).strftime('%Y%m%d')
 
-        save_location = f"{path_figures}{weights}/{year}/{month}/"
+        save_location = f"{path_figures}{weights}/single_sample/"
         if not os.path.exists(save_location):
             os.makedirs(save_location)
 
@@ -89,46 +90,50 @@ def main():
         fig = plt.figure(figsize=(20,20))
         # fig.subplots_adjust(bottom = 0.2)
         ax = plt.axes(projection=map_proj)
-        ax.set_title(f"Forecast for two day forecast initiated {init_date}", fontsize = 30)
+        # ax.set_title(f"Forecast for two day forecast initiated {init_date}", fontsize = 30)
     
         ax.pcolormesh(lon, lat, y_pred, transform=data_proj, norm = ice_norm, cmap = ice_cmap, zorder=1)
-        ax.pcolormesh(lon, lat, np.ma.masked_less(lsmask, 1), transform=data_proj, zorder=2, cmap='autumn')
+        # ax.pcolormesh(lon, lat, np.ma.masked_less(lsmask, 1), transform=data_proj, zorder=2, cmap='autumn')
 
         # cbar_ax = fig.add_axes([0.15, 0.1, 0.6, 0.025])
-        mapper = mpl.cm.ScalarMappable(cmap = ice_cmap, norm = ice_norm)
+        # mapper = mpl.cm.ScalarMappable(cmap = ice_cmap, norm = ice_norm)
         # mapper.set_array([-1, 8])
 
-        cbar = fig.colorbar(mapper,
-                            ax = ax,
-                            spacing = 'uniform',
-                            location = 'bottom',
-                            orientation = 'horizontal',
-                            shrink = .7,
-                            pad = .05
-        )
+        # cbar = fig.colorbar(mapper,
+                            # ax = ax,
+                            # spacing = 'uniform',
+                            # location = 'bottom',
+                            # orientation = 'horizontal',
+                            # shrink = .7,
+                            # pad = .05
+        # )
 
-        cbar.set_label(label = 'SIC range [%]', size = 16)
-        cbar.set_ticks(ice_levels[:-1] + .5, labels = ice_ticks)
-        cbar.ax.tick_params(labelsize = 16)
+        # cbar.set_label(label = 'SIC range [%]', size = 16)
+        # cbar.set_ticks(ice_levels[:-1] + .5, labels = ice_ticks)
+        # cbar.ax.tick_params(labelsize = 16)
 
         ax.set_xlim(x0,x1)
         ax.set_ylim(y0,y1)
-
+        
         fig.canvas.draw()
         ax.gridlines(xlocs = xticks, ylocs = yticks, color = 'dimgrey')
         ax.xaxis.set_major_formatter(LONGITUDE_FORMATTER)
         ax.yaxis.set_major_formatter(LATITUDE_FORMATTER)
         LambertLabels.lambert_xticks(ax, xticks)
         LambertLabels.lambert_yticks(ax, yticks)
+        ax.add_feature(cfeature.COASTLINE, lw=2)
 
 
-        plt.savefig(f"{save_location}{yyyymmdd}.png")
+        ax.get_xaxis().set_visible(False)
+        ax.get_yaxis().set_visible(False)
+
+        plt.savefig(f"{save_location}{yyyymmdd}.png", bbox_inches = 'tight', pad_inches = 0)
 
         f_model.close()
         ax.cla()
         plt.close(fig)
         
-
+        exit()
 
 if __name__ == "__main__":
     main()
